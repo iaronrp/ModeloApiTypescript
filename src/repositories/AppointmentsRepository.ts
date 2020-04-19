99145fae5a09073611a472a107d317e2e26b1697
Iaron Aires <iaronrp@hotmail.com>
2020-04-19 09:05:10 -0300

    Api Typescript

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,32 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+class AppointmentsRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public all(): Appointment[] {
+    return this.appointments;
+  }
+
+  public create(provider: string, date: Date): Appointment {
+    const appointment = new Appointment(provider, date);
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+
+    return findAppointment || null;
+  }
+}
+
+export default AppointmentsRepository;
---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+import routes from './routes';
+
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('Server Started on port 3333!');
+});
