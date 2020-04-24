be4d81e740b7114c66ac4f7353520afa6189cee6
Iaron Aires <iaronrp@hotmail.com>
2020-04-23 21:08:34 -0300

    Api Typescript v3

--- src/database/migrations/1587678246581-CreateUsers.ts
+++ src/database/migrations/1587678246581-CreateUsers.ts
@@ -1,11 +1,10 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
-export default class CreateAppointment1587567902267
-  implements MigrationInterface {
+export default class CreateUsers1587678246581 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -15,14 +14,19 @@ export default class CreateAppointment1587567902267
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
             isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
             isNullable: false,
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
@@ -40,6 +44,6 @@ export default class CreateAppointment1587567902267
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import appointmentsRouter from './appointments.routes';
+import usersRoutes from './users.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRoutes);
 
 export default routes;
